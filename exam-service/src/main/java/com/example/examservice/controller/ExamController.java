a37fc4cd31574553eb036275561a4b9561a631ad
MikalaiVakar <mikalai.vakar@innowise-group.com>
2022-05-15 15:51:59 +0300

    added specs list

--- exam-service/src/main/java/com/example/examservice/controller/ExamController.java
+++ exam-service/src/main/java/com/example/examservice/controller/ExamController.java
@@ -6,13 +6,16 @@ import com.example.examservice.service.ExamService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -20,6 +23,9 @@ import java.util.Map;
 @RequiredArgsConstructor
 @Slf4j
 public class ExamController {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
 
     @Autowired
     ExamService examService;
@@ -32,5 +38,12 @@ public class ExamController {
     @PostMapping("/create")
     public ResponseEntity<Question> getExam(@RequestParam String spec, @RequestBody Question question) {
         return examService.createQuestion(spec, question);
+    }
+
+    @GetMapping("/specs")
+    public List<String> getSpecs() {
+        List<String> specs = discoveryClient.getServices();
+        specs.remove("exam-service");
+        return specs;
     }
 }
